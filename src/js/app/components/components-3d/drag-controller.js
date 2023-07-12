737649e46fc2e97ae1a758e6d9087d9dfb4b6310
MladenovaKristina <kristinam@crazylabs.com>
2023-07-12 13:39:36 +0200

    HOTFIX: Changed move range logic

--- src/js/app/components/components-3d/drag-controller.js
+++ src/js/app/components/components-3d/drag-controller.js
@@ -7,12 +7,7 @@ export default class DragController extends THREE.Object3D {
         this._scene = scene;
         this._raycasterPlane = raycasterPlane;
         this._object = null;
-        this._initView();
-    }
-
-    _initView() {
         this._raycaster = new THREE.Raycaster();
-        this._pointer = new THREE.Vector2();
     }
 
     viewObject(object) {
@@ -21,7 +16,6 @@ export default class DragController extends THREE.Object3D {
 
     onDown(x, y) {
         this.move(x, y);
-
     }
 
     onMove(x, y) {
@@ -33,25 +27,32 @@ export default class DragController extends THREE.Object3D {
     }
 
     move(x, y) {
+        const screenWidth = window.innerWidth;
+        const screenHeight = window.innerHeight;
 
-        this._pointer.x = (x / window.innerWidth) * 2 - 1;
-        this._pointer.y = -(y / window.innerHeight) * 2 + 1;
+        const clampedX = Math.max(Math.min(x, screenWidth), 0);
+        const clampedY = Math.max(Math.min(y, screenHeight), 0);
 
-        this._raycaster.setFromCamera(this._pointer, this._camera);
+        const normalizedX = (clampedX / screenWidth) * 2 - 1;
+        const normalizedY = -(clampedY / screenHeight) * 2 + 1;
+
+        this._raycaster.setFromCamera({ x: normalizedX, y: normalizedY }, this._camera);
         const intersects = this._raycaster.intersectObjects(this._scene.children, true).filter(object => object.object.geometry.type === 'BoxGeometry');
 
-        if (intersects != 0) {
+        if (intersects.length > 0) {
             this.color();
-            this._object.position.x = this._pointer.x;
-            this._object.position.y = this._pointer.y;
-        } else return;
+            // Get the intersection point in world coordinates
+            const intersectionPoint = intersects[0].point;
+            // Set the object's position
+            this._object.position.copy(intersectionPoint).setZ(0);
+        }
     }
 
     color() {
-        this._object.children[0].material.color.set(0x00ff00)
+        this._object.children[0].material.color.set(0x00ff00);
     }
 
     reset() {
-        this._object.children[0].material.color.set(0xff0000)
+        this._object.children[0].material.color.set(0xff0000);
     }
 }
